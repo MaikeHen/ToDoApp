9d5c82309695b22bbdc05e96f12aaea778a886af
Maike Henze <maike.henze@gmail.com>
2023-09-12 16:44:03 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const allTodos = document.querySelector("#all");
+const openTodos = document.querySelector("#open");
+const doneTodos = document.querySelector("#done");
+
+const removeBtn = document.querySelector("button");
+
+const addTodos = document.querySelector("#addTodo");
+const addBtn = document.querySelector("#addBtn");
